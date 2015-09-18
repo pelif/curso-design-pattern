6b82346d57daa7ef00e1f85e7bd29693d0b32ee1
fdoall <felipe.oliveira@allink.com.br>
2015-09-18 17:22:31 -0300

    add factory

---
+++ factory/AbstractDatabaseConnection.php
@@ -0,0 +1,26 @@
+<?php
+namespace padroes\criacao\factory;
+
+use padroes\criacao\factory\IDatabaseConnection;
+
+abstract class AbstractDatabaseConnection implements IDatabaseConnection{
+
+  public function __construct($username = null, $password = null, $dbname = null, $host = null, $port = null)
+  {
+    if ($username && $password && $dbname)
+      $this->connect($username, $password, $dbname, $host);
+  }
+
+  public function connect($username, $password, $dbname, $host, $port = null)
+  {
+
+  }
+
+  public function insert($model){}
+  public function update($model){}
+  public function delete($model){}
+  public function select($criteria)
+  {
+    return null;
+  }
+}
